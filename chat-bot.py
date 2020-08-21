d0e1d8747dbbae9af919042fd25b6429ebcc239a
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2020-08-21 14:27:48 -0300

    primeiro commit

---
+++ chat-bot.py
@@ -0,0 +1,30 @@
+from time import sleep
+while True:
+    oi = input("Oi? ")
+    if oi == "oi":
+        bem = input("Tudo bem? ")
+        if bem in "SIM estou claro ".upper() or bem == "Estou indo".upper() or bem == "estou bem".upper():
+            print("OK")
+
+        nome = input("Qual seu nome? ")
+        sleep(1)
+        print("O meu é jissele")
+        sleep(1)
+        if nome == "deivid":
+            print("belo nome")
+        break
+    else: print("?",sep=" ") #sep é para pular um alinha
+
+
+
+while True:
+    bem = input(f"Olá {nome}, Tudo bem? ").title()
+    if bem == "sim".title():
+        print("legal")
+        break
+    elif bem == "nao".title():
+        print("Espero que fique bem!")
+        break
+    else: print("\033[1;31mnão estou programado para responder isso!!!\033[m tente sim ou nao,",sep=" ")
+
+dialogo = input("")
